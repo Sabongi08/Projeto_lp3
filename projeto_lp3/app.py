d4ef78f0ebc2559f1b294bbed20e8fbc6ca8a9d5
Débora <d.sabongi@gmail.com>
2024-06-18 11:31:04 -0300

    aula de hj

--- projeto_lp3/app.py
+++ projeto_lp3/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from validate_docbr import CPF, CNPJ
 
 app = Flask("Minha aplicação")
@@ -7,19 +7,39 @@ app = Flask("Minha aplicação")
 #  rota + função 
 
 # / -home page 
-@app.route("/")
-def home ():
-    return "<h1>Home Page </h1>"
+# @app.route("/")
+# def home ():
+#    return "<h1>Home Page </h1>"
 
-# /contato - pg de contato 
+# / -contato  page 
 @app.route("/contato")
-def contato():
-    return "<h1>Contato</h1>"
+def contato ():
+    return render_template ("contato.html")
 
-# /produtos - pg produtos 
 @app.route("/produtos")
 def produtos ():
-    return "<h1>Produtos</h1>"
+    lista_produtos = [
+        {"nome": "coca-cola", "descricao": "Mata a sede "},
+        {"nome": "doritos", "descricao":"seja a tua mao "},
+        {"nome": "chocolate", "descricao":"bonzao "},
+    ]
+    
+    return render_template ("produtos.html", produtos =lista_produtos)
+
+
+@app.route("/home")
+def home ():
+    return render_template ("home.html")
+
+# /contato - pg de contato 
+# @app.route("/contato")
+# def contato():
+#     return "<h1>Contato</h1>"
+
+# /produtos - pg produtos 
+# @app.route("/produtos")
+# def produtos ():
+#     return "<h1>Produtos</h1>"
 
 # pagina /servicos retornar "Nossos serviços"
 @app.route("/servicos")
